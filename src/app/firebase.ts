0e5ce3a937d01aa9040b99e3a5bfd75c73af35b1
rajitha-00 <rajithatw07102@gmail.com>
2026-09-09 12:57:11 +0530

    deploy

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDiq3VYwX4ImwTMf6GTTBkhRPFOBWlkklc",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "hotel-os-2edf7.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "hotel-os-2edf7",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "hotel-os-2edf7.appspot.com",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "82816171415",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:82816171415:web:06c769bbc830e9500018d2",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
